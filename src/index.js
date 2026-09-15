fe7fd6c85fb3171189a8c8c0499ca35d47b9e3f6
Lucas Daniel Andreatta <danielandreatta2@gmail.com>
2026-09-15 15:56:29 -0300

    index.js , inicio.ejs y main.ejs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("node:path");
 const { leerJson } = require("./archivos");
+const expressLayouts = require("express-ejs-layouts");
 
 const PORT = 3000;
 const rutaDatos = path.join(__dirname, "..", "datos", "mascotas.json");
@@ -8,6 +9,17 @@ const rutaDatos = path.join(__dirname, "..", "datos", "mascotas.json");
 async function main() {
     const mascotas = await leerJson(rutaDatos);
     const app = express();
+
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "..", "views"));
+
+    app.use(expressLayouts);
+    app.set("layout", "layouts/main");
+
+    app.get("/", (req, res) => {
+        res.render("inicio", { titulo: "Refugio de Animales" });
+    });
+
     app.get("/api/mascotas", (req, res) => {
         res.json(mascotas);
     });
